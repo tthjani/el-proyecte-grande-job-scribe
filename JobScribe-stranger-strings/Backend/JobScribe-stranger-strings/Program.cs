dd906f4bb5da549770e16bcbb14e5ea8189570ca
tthjani <tothje98@gmail.com>
2023-12-07 13:52:05 +0100

    create: added frontend via "Vite, React", connected backend and frontend

--- JobScribe-stranger-strings/Backend/JobScribe-stranger-strings/Program.cs
+++ JobScribe-stranger-strings/Backend/JobScribe-stranger-strings/Program.cs
@@ -3,6 +3,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors(options => options.AddPolicy("CorsPolicy",
+    builder =>
+    {
+        builder
+            .WithOrigins("http://localhost:5173")                        
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials();
+    }));    
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,6 +26,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
