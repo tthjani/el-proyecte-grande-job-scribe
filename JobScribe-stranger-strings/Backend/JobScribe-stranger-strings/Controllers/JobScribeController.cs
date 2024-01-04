ffcedb56c2d23925731ac64359fc806627b6ffdd
denes-bartfai <denes.bartfai@gmail.com>
2024-01-04 11:30:36 +0100

    feat: create a get request for company

--- JobScribe-stranger-strings/Backend/JobScribe-stranger-strings/Controllers/JobScribeController.cs
+++ JobScribe-stranger-strings/Backend/JobScribe-stranger-strings/Controllers/JobScribeController.cs
@@ -1,4 +1,7 @@
+using JobScribe_stranger_strings.Data;
+using JobScribe_stranger_strings.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace JobScribe_stranger_strings.Controllers;
 
@@ -22,5 +25,26 @@ public class JobScribeController : ControllerBase
             "Successful connection to the server!"
         };
         return Ok(respond);
+    }
+
+    [HttpGet("GetCompany")]
+    public async Task<ActionResult<Company>> GetCompany(string companyName)
+    {
+        try
+        {
+            await using var dbContext = new JobScribeContext();
+            var company = dbContext.Companies.FirstOrDefault(c => c.Name == companyName);
+
+            if (company == null)
+            {
+                return NotFound($"Company {companyName} not found");
+            }
+
+            return Ok(company);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "An error occured");
+        }
     }
 }
