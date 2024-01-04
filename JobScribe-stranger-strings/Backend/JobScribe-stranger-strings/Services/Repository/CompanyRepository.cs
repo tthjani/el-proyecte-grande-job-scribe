d54a81a2dadeb57fdc144cd360a49f5fc3aa29d0
Tóth Zoltán <toth.zoltan8719@gmail.com>
2024-01-04 16:19:55 +0100

    feat: implementing ICompanyRepository interface

---
+++ JobScribe-stranger-strings/Backend/JobScribe-stranger-strings/Services/Repository/CompanyRepository.cs
@@ -0,0 +1,33 @@
+using JobScribe_stranger_strings.Data;
+using JobScribe_stranger_strings.Model;
+
+namespace JobScribe_stranger_strings.Services.Repository;
+
+public class CompanyRepository : ICompanyRepository
+{
+    public IEnumerable<Company> GetAll()
+    {
+        using var dbContext = new JobScribeContext();
+        return dbContext.Companies.ToList();
+    }
+
+    public Company? GetByName(string name)
+    {
+        using var dbContext = new JobScribeContext();
+        return dbContext.Companies.FirstOrDefault(c => c.Name == name);
+    }
+
+    public void Add(Company company)
+    {
+        using var dbContext = new JobScribeContext();
+        dbContext.Add(company);
+        dbContext.SaveChanges();
+    }
+
+    public void Delete(Company company)
+    {
+        using var dbContext = new JobScribeContext();
+        dbContext.Remove(company);
+        dbContext.SaveChanges();
+    }
+}
