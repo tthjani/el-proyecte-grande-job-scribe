1b09392d48a8bb59f6098887f367315cd28674f6
denes-bartfai <denes.bartfai@gmail.com>
2024-01-17 22:45:40 +0100

    update: AuthService to able to pass the role along with the registration info

--- JobScribe-stranger-strings/Backend/JobScribe-stranger-strings/Services/Authentication/AuthService.cs
+++ JobScribe-stranger-strings/Backend/JobScribe-stranger-strings/Services/Authentication/AuthService.cs
@@ -14,16 +14,17 @@ public class AuthService : IAuthService
         _tokenService = tokenService;
     }
     
-    public async Task<AuthResult> RegisterAsync(string email, string username, string password)
+    public async Task<AuthResult> RegisterAsync(string email, string username, string password, string role)
     {
-        var result = await _userManager.CreateAsync(
-            new IdentityUser { UserName = username, Email = email }, password);
+        var user = new IdentityUser { UserName = username, Email = email };
+        var result = await _userManager.CreateAsync(user, password);
 
         if (!result.Succeeded)
         {
             return FailedRegistration(result, email, username);
         }
-
+        
+        await _userManager.AddToRoleAsync(user, role);
         return new AuthResult(true, email, username, "");
     }
 
--- JobScribe-stranger-strings/Backend/JobScribe-stranger-strings/Services/Authentication/IAuthService.cs
+++ JobScribe-stranger-strings/Backend/JobScribe-stranger-strings/Services/Authentication/IAuthService.cs
@@ -2,6 +2,6 @@ namespace JobScribe_stranger_strings.Services.Authentication;
 
 public interface IAuthService
 {
-    Task<AuthResult> RegisterAsync(string email, string username, string password);
+    Task<AuthResult> RegisterAsync(string email, string username, string password, string role);
     Task<AuthResult> LoginAsync(string username, string password);
 }
