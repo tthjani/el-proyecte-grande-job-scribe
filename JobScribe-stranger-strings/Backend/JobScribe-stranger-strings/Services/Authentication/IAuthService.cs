577bf63de74a1ca6fda629c5a350c171706ba98a
denes-bartfai <denes.bartfai@gmail.com>
2024-01-17 21:11:37 +0100

    feat: add the login functionality to the AuthService

--- JobScribe-stranger-strings/Backend/JobScribe-stranger-strings/Services/Authentication/IAuthService.cs
+++ JobScribe-stranger-strings/Backend/JobScribe-stranger-strings/Services/Authentication/IAuthService.cs
@@ -3,4 +3,5 @@ namespace JobScribe_stranger_strings.Services.Authentication;
 public interface IAuthService
 {
     Task<AuthResult> RegisterAsync(string email, string username, string password);
+    Task<AuthResult> LoginAsync(string username, string password);
 }
