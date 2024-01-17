fe78d4fb1a6bd99aae8ba01647f7cccbec64153d
denes-bartfai <denes.bartfai@gmail.com>
2024-01-17 20:31:11 +0100

    feat: create AuthController class with post request and error handling

---
+++ JobScribe-stranger-strings/Backend/JobScribe-stranger-strings/Controllers/AuthController.cs
@@ -0,0 +1,42 @@
+﻿using JobScribe_stranger_strings.Contracts;
+using JobScribe_stranger_strings.Services.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobScribe_stranger_strings.Controllers;
+
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService _authenticationService;
+
+    public AuthController(IAuthService authenticationService)
+    {
+        _authenticationService = authenticationService;
+    }
+    
+    [HttpPost("Register")]
+    public async Task<ActionResult<RegistrationResponse>> Register(RegistrationRequest request)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var result = await _authenticationService.RegisterAsync(request.Email, request.Username, request.Password);
+
+        if (!result.Success)
+        {
+            AddErrors(result);
+            return BadRequest(ModelState);
+        }
+
+        return CreatedAtAction(nameof(Register), new RegistrationResponse(result.Email, result.UserName));
+    }
+
+    private void AddErrors(AuthResult result)
+    {
+        foreach (var error in result.ErrorMessages)
+        {
+            ModelState.AddModelError(error.Key, error.Value);
+        }
+    }
+}
