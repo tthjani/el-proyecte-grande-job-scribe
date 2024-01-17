5bbf6e0d001e07fb85fcc500fb117de26e529852
tthjani <tothje98@gmail.com>
2024-01-17 13:34:47 +0100

    refact: removed redundant properties, changed IList to List

--- JobScribe-stranger-strings/Backend/JobScribe-stranger-strings/Model/JobOffer.cs
+++ JobScribe-stranger-strings/Backend/JobScribe-stranger-strings/Model/JobOffer.cs
@@ -3,15 +3,15 @@ namespace JobScribe_stranger_strings.Model;
 public class JobOffer
 {
     public int Id { get; set; }
-    public Company Company { get; set; }
+    public int CompanyId { get; set; }
     public string Name { get; set; }
     public DateTime Published { get; set; }
     public string Location { get; set; }
     public string Level { get; set; }
     public string Category { get; set; }
     public string Description { get; set; }
-    public IList<string> MustHave { get; set; }
-    public IList<string> NiceToHave { get; set; }
-    public IList<Applicant>? Applicants { get; set; }
+    public int ApplicantId { get; set; }
     public bool IsActive { get; set; }
+    public List<MustHaveModel> MustHave { get; set; }
+    public List<NiceToHaveModel> NiceToHave { get; set; }
 }
