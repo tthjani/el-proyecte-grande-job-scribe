edcfd0e054fb1d667b624fc447671316caa83675
tthjani <tothje98@gmail.com>
2024-01-29 15:39:44 +0100

    refact: cleaned, shorted App.jsx

--- JobScribe-stranger-strings/Frontend/src/App.jsx
+++ JobScribe-stranger-strings/Frontend/src/App.jsx
@@ -1,58 +1,20 @@
-import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Navbar from "./Components/Navbar.jsx";
+import Home from "./Components/Home";
+import CompanyRegistration from "./Components/CompanyReg";
+import Registration from "./Components/Registration";
 
 
 function App() {
-  const [companyData, setCompanyData] = useState("");
-
-  function getData() {
-    fetch("http://localhost:5225/api/Company/GetAllCompanies")
-      .then((response) => response.json())
-      .then((data) => setCompanyData(data.res))
-      .catch((error) => console.error(error));
-  }
-
-
+  
 
   return (
     <>
-      <header>
-        <Navbar />
-      </header>
-      <h1>Welcome to JobScribe basic frontend!</h1>
-      <div className="card">
-        <button onClick={() => getData()}></button>
-        {Array.isArray(companyData) && companyData.length > 0 ? (
-          <table>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Location</th>
-                <th>Industry</th>
-                <th>Founded</th>
-                <th>Description</th>
-              </tr>
-            </thead>
-            <tbody>
-              {companyData.map((company) => (
-                <tr key={company.id}>
-                  <td>{company.name}</td>
-                  <td>{company.location}</td>
-                  <td>{company.industry}</td>
-                  <td>{company.founded}</td>
-                  <td>{company.description}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        ) : (
-          <p>No companies available.</p>
-        )}
-      </div>
-      <p className="read-the-docs">
-        The site is under development! Check back later!
-      </p>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/userregistration" element={<Registration />} />
+        <Route path="/companyregistration" element={<CompanyRegistration />} />
+      </Routes>
     </>
   );
 }
