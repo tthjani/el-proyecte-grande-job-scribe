98a797bf302524854114d64f38651f38d493888e
tthjani <tothje98@gmail.com>
2024-01-31 09:50:28 +0100

    feat: added userlogin

---
+++ JobScribe-stranger-strings/Frontend/src/Components/UserLogin.jsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const UserLogin = () => {
+  const [Password, setPassword] = useState("");
+  const [Email, setEmail] = useState("");
+  const [succesfullLogin, setSuccesfullLogin] = useState(false);
+
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const data = { Email, Password };
+    fetch("http://localhost:5225/UserLogin", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    })
+      .then((res) => {
+        console.log(JSON.stringify(data));
+        console.log("Status: " + res.status);
+        if (res.status === 200) {
+          setSuccesfullLogin(true);
+          console.log("Login sucessfully");
+          alert("Succesfull Login!");
+        } else {
+          console.log("Login failed");
+        }
+      })
+      .catch((error) => {
+        console.error("Connection error:", error);
+      });
+  };
+
+  return (
+    <div>
+      {succesfullLogin ? (
+        <h1> Login succesfull! </h1>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <h3>Login as jh</h3>
+          <label>E-mail</label>
+          <input
+            name="email"
+            placeholder="Enter email"
+            value={Email}
+            onChange={handleEmailChange}
+            id="email"
+          />
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter password"
+            value={Password}
+            onChange={handlePasswordChange}
+            id="password"
+          />
+          <button
+            type="submit"
+            className="reg-button"
+            value="Submit"
+          >
+            Submit
+          </button>
+        </form>
+      )}
+      <button>
+        <Link to="/">Home</Link>
+      </button>
+    </div>
+  );
+};
+
+export default UserLogin;
