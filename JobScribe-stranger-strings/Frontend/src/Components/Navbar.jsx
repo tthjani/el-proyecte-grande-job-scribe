f9f344eebd6822781811ec1b14bcb49c6164774c
tthjani <tothje98@gmail.com>
2024-02-01 20:43:50 +0100

    feat: added logout compare to the navbar

--- JobScribe-stranger-strings/Frontend/src/Components/Navbar.jsx
+++ JobScribe-stranger-strings/Frontend/src/Components/Navbar.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-function Navbar() {
+function Navbar({isLoggedIn, setIsLoggedIn}) {
   const [registerDropdown, setRegisterDropdown] = useState(false);
   const [loginDropdown, setLoginDropdown] = useState(false);
+  const navigate = useNavigate();
 
   const toggleRegisterDropdown = () => {
     setRegisterDropdown(!registerDropdown);
@@ -15,14 +16,28 @@ function Navbar() {
     setRegisterDropdown(false);
   };
 
+  const handleLogout = async () => {
+    try {
+      setIsLoggedIn(false); 
+      navigate("/logout");
+    } catch (error) {
+      console.error("Something went wrong with logout...", error);
+    }
+  };
 
   return (
     <div>
       <nav className="navbar">
         <div className="dropdownReg">
-          <button className="regButton" onClick={toggleRegisterDropdown}>
-            Register
-          </button>
+          {isLoggedIn ?  (
+            <button className="regButton">
+              <Link to="/profile">Profile</Link>
+            </button>
+          ) : (
+            <button className="regButton" onClick={toggleRegisterDropdown}>
+              Register
+            </button>
+          )}
           {registerDropdown && (
             <div className="regDrop">
               <button>
@@ -36,15 +51,23 @@ function Navbar() {
         </div>
         <button className="jobButton">Job Offers</button>
         <div className="dropdownLog">
-          <button className="loginButton" onClick={toggleLoginDropdown}>
-            Login
-          </button>
+          {isLoggedIn ? (
+            <button className="loginButton" onClick={handleLogout}>
+              Logout
+            </button>
+          ) : (
+            <button className="loginButton" onClick={toggleLoginDropdown}>
+              Login
+            </button>
+          )}
           {loginDropdown && (
             <div className="loginDrop">
               <button>
                 <Link to="/userlogin">Login as Jobhunter</Link>
               </button>
-              <button>Login as Company</button>
+              <button>
+                <Link to="/companylogin">Login as Company</Link>
+              </button>
             </div>
           )}
         </div>
